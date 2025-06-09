c0231cdf442710a55df8aec9e7efc603cd8eb9da
gaaramk <gaarakml@gmail.com>
2025-06-09 05:22:51 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/zekV2",
+  base: "/zekV2/",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
